6887b4395776c02bf1d94f2bef82eedca9bae181
bmolina1993 <breno.m.c@hotmail.com>
2025-05-08 02:24:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -164,5 +164,6 @@ def download_relatorio():
 if __name__ == '__main__':
     import os
 port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
+app.run(host="0.0.0.0", port=port, debug=False)
+
 
